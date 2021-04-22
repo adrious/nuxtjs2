4ca77fb59667d2ed85bc5ae8fdb8627d503617be
Adrian Arroyos <adrian@catalysts.net>
2021-04-22 12:54:47 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         nuxtjs
       </h1>
       <h2 class="subtitle">
-        My astonishing Nuxt.js project
+        Not just ANY astonishing Nuxt.js project....MINE!
       </h2>
       <div class="links">
         <a
